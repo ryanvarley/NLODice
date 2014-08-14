da6d640864892a82adaf3136b1f58f0597c43c54
Ryan Varley <ryanjvarley@gmail.com>
2014-08-14 20:00:01 +0100

    initial commit (project outline)

---
+++ config-example.py
@@ -0,0 +1,7 @@
+""" Copy this file to config.py and add your values to each field
+"""
+
+rpcip = '127.0.0.1'  # this is localhost
+rpcport = 'rpcport'
+rpcuser = 'rpcuser'
+rpcpass = 'rpcpass'
---
+++ run.py
@@ -0,0 +1,18 @@
+""" A simple dice betting game to check nlocoin transactions through rpc
+"""
+
+# TODO connect to DB
+
+# TODO setup RPC
+
+# Per address / odds
+
+# TODO check transactions (and that none are missed)
+
+# TODO process unrolled transactions
+
+    # TODO roll dice
+
+    # TODO info to DB
+
+    # TODO transaction
