88ecd8be92d61b7ed6204be8dec6ee7ab6357478
Ryan Varley <ryanjvarley@gmail.com>
2014-08-14 20:14:28 +0100

    setup and connect to sqlite db

--- run.py
+++ run.py
@@ -1,7 +1,25 @@
 """ A simple dice betting game to check nlocoin transactions through rpc
 """
+from config import *
+from peewee import *
 
-# TODO connect to DB
+db = SqliteDatabase(sqlite_location)
+
+
+class DiceTransactions(Model):
+    roll_id = PrimaryKeyField()
+    from_address = CharField(34)
+    txin_id = CharField(64)
+    txin_amount = DecimalField()
+    win = BooleanField()
+    txout_id = CharField(64)
+    txout_amount = DecimalField()
+
+    class Meta:
+        database = db  # This model uses the "people.db" database.
+
+if 0:  # on first run create the tables
+    db.create_tables([DiceTransactions])
 
 # TODO setup RPC
 
